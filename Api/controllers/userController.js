f722c573a39b7143f4aae29a53a6f0e849f8f2f4
ahmed ibrahim <99619119+ahmed1567@users.noreply.github.com>
2023-05-13 02:55:30 +0300

    delete account option

--- Api/controllers/userController.js
+++ Api/controllers/userController.js
@@ -1,9 +1,27 @@
-const post = async (req, res) => {
-    return res.send("dsdffdfdfd")
+const {User} =require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const {errorHandler}=require("../Helpers/errorHandler")
+
+
+const deleteUser = async (req, res,next) => {
+    if(req.params.id.length !==24){
+      next(errorHandler(400,"please enter valid id"))
+    }
+    const user = await User.findById(req.params.id)
+    
+    if(!user)return res.send("User Not found")
+    
+    if(req.userId !== user._id.toString()){
+    return next(errorHandler(403,"You can only delete your account"))
+    } 
+   
+    await User.findByIdAndDelete(req.params.id)
+    res.send("deleted")
+
   };
 
 
   module.exports = {
-    post
+    deleteUser
 
   };
