d98b58457744096bd157b0df3be92f19240e4bcb
ahmed ibrahim <99619119+ahmed1567@users.noreply.github.com>
2023-05-16 15:47:16 +0300

    conversations routes and controller

--- Api/routes/conversation.js
+++ Api/routes/conversation.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const userController = require("../controllers/conversationController");
+const {getConversations,createConversation,getSingleConversation,updateConversation} = require("../controllers/conversationController");
+const { verifyToken } = require("../middlewares/jwt");
 
 
-router.get("/post", userController.post);
+router.get("/", verifyToken, getConversations);
+router.post("/", verifyToken, createConversation);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.put("/:id", verifyToken, updateConversation);
 
 
 module.exports = router;
